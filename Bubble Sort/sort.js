c70665144fd4f77080526251c8ff5008f98c7654
Dhananjayan B <75248395+dhananjayan573@users.noreply.github.com>
2021-02-23 17:47:39 +0530

    changed everything

--- Bubble Sort/sort.js
+++ Bubble Sort/sort.js
@@ -1,25 +1,19 @@
-function swap() {
-    var a, n, i, j, t;
-    const v1 = document.getElementsByTagName("input")[0].value;
-    const v2 = document.getElementsByTagName("input")[1].value;
-    const v3 = {
-        for (i = 0; i < n; i++) {
-            console.log(a.length);
-        }
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n - 1; j++) {
-                if (a[j] >= a[j + 1]) {
-                    t = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = t;
-                }
+var a = new Array;
+
+function sort() {
+    for (i = 0; i < 5; i++) {
+        a[i] = document.getElementsByTagName("input")[i].value;
+    }
+    document.write("<h2>Given Array :</h2><br>" + a);
+
+    for (i = 0; i <= a.length; i++) {
+        for (j = 0; j < a.length; j++) {
+            if (a[j] > a[j+1]) {
+                var temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
-        console.log(a[i]);
-
-        for (i = 0; i < n; i++) {
-            console.log(a[i]);
-        }
     }
-    document.getElementsByTagName("input")[2].value = v3;
+    document.write("<h2>Sorted Array :</h2><br>" + a);
 }
